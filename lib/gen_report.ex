6005b28638b46a4290ffecbd5682a4ccff5a80d8
Paulo Victor Rios <victorios.ba@gmail.com>
2021-12-07 08:18:59 -0300

    second challenge

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -41,6 +41,56 @@ defmodule GenReport do
     {:error, "Insira o nome de um arquivo"}
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report ->
+        sum_reports(report, result)
+      end)
+
+    {:ok, result}
+  end
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => hours_per_month1,
+           "hours_per_year" => hours_per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => hours_per_month2,
+           "hours_per_year" => hours_per_year2
+         }
+       ) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+    hours_per_month = deep_merge(hours_per_month1, hours_per_month2)
+    hours_per_year = deep_merge(hours_per_year1, hours_per_year2)
+
+    build_reports(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
+  defp deep_merge(left, right) do
+    Map.merge(left, right, &deep_resolve/3)
+  end
+
+  defp deep_resolve(_key, left = %{}, right = %{}) do
+    merge_maps(left, right)
+  end
+
+  defp deep_resolve(_key, _left, right) do
+    right
+  end
+
   defp report_acc() do
     months = Enum.into(@months, %{}, &{&1, 0})
     years = Enum.into(2016..2020, %{}, &{&1, 0})
